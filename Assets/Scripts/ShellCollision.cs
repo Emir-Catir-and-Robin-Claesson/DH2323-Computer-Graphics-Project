67dce010ed846f81f7ba6ce7bb646e02ffaf0eea
Emir Catir <78615181+empazi@users.noreply.github.com>
2024-05-09 13:25:59 +0200

    Added baseproject
    Lab3 as base for project

---
+++ Assets/Scripts/ShellCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellCollision : MonoBehaviour
+{
+    public GameObject explosionParticlesPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Destroy the shell if it hits something (e.g. rock, ground, enemytank, etc.). 
+        // If the shell hits enemy tank, the enemy tank will also be destroyed.
+
+        // Your code here.
+        if (collision.collider.tag != "Player")
+        {
+            //Debug.Log($"{collision.collider.name}");
+            Destroy(gameObject);
+            if (explosionParticlesPrefab != null)
+            {
+                GameObject explosion = (GameObject)Instantiate(explosionParticlesPrefab, transform.position, explosionParticlesPrefab.transform.rotation);
+                Destroy(explosion, explosion.GetComponent<ParticleSystem>().main.startLifetimeMultiplier);
+            }
+
+            if(collision.collider.tag == "Enemy")
+            {
+                Destroy(collision.collider.gameObject);
+            }
+        }
+    }
+
+}
